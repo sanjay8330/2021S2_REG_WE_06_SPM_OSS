0297c378c21c7a679c7d439c0bf735300f05772e
sanjay8330 <sanjayabi8330@gmail.com>
2021-07-15 07:26:58 +0530

    Commit as at 15/7 - Frontend & Backend user change

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,9 @@ import Home from './home';
 import Footer from './components/footer/footer';
 import Register from './components/userManagement/register';
 import Login from './components/userManagement/login';
+import ForgotPassword from './components/userManagement/forgotPassword';
+import ResetPassword from './components/userManagement/resetPassword';
+
 import adminDashboard from '../src/adminDashboard';
 import createProduct from './components/productManagement/createProduct';
 import ViewProducts from './components/productManagement/viewProducts';
@@ -17,6 +20,9 @@ function App() {
             <Route path="/" component={Home} exact />
             <Route path="/register" component={Register} />
             <Route path="/login" component={ Login } />
+            <Route path="/forgotPassword" component={ ForgotPassword } />
+            <Route path="/resetPassword/:id" component={ ResetPassword } />
+
             <Route path="/adminDashboard" component={ adminDashboard } />
             <Route path="/createProduct" component={ createProduct } />
             <Route path="/viewProducts" component={ ViewProducts } />
