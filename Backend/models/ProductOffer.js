5bbca20b543bb3385bbeab4e6c5e846ac0cf7015
sanjay8330 <sanjayabi8330@gmail.com>
2021-08-09 21:29:45 +0530

    Commit to test from my new device

--- Backend/models/ProductOffer.js
+++ Backend/models/ProductOffer.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const ProductOfferSchema = new mongoose.Schema({
+    products : {
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref: 'Products'
+    },
     offerPrice: {
         type: Number,
         required: true
