aaa902fe2bbd1019968e18f56d1fb8452c5a4599
kasunavodya@gmail.com <kasunavodya@gmail.com>
2021-09-15 19:22:31 +0530

    resolve backend conflicts

--- Backend/routes/Checkout.js
+++ Backend/routes/Checkout.js
@@ -17,10 +17,6 @@ router.route('/paymentDetails/:userId/:amount').post(async (req, res) => {
 //Get payment history by userId - CUSTOMER PAYMENT HISTORY
 router.route("/readHistoryForCustomer/:userId").get(async (req, res) => {
     const currentuserId = req.params.userId;
-<<<<<<< HEAD
-    //const currentDate = req.params.date;
-=======
->>>>>>> ab85ea6bd36e7f8750c5d0b8f76539d13f64878b
 
     CheckoutModel.find({ userId: currentuserId})
         .then(data => {
